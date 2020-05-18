113e8360b6ace30a931d4d074ae23ed99358b2b2
gracelove <govlmo91@gmail.com>
2020-05-18 19:43:59 +0900

    order 폼

--- src/main/java/kr/gracelove/tacocloud/tacos/web/DesignTacoController.java
+++ src/main/java/kr/gracelove/tacocloud/tacos/web/DesignTacoController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -45,6 +46,13 @@ public class DesignTacoController {
         model.addAttribute("taco", new Taco());
 
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(Taco design) {
+        log.info("Processing design: " + design);
+
+        return "redirect:/orders/current";
     }
 
     private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type) {
