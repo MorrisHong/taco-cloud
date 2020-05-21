40967bc4d6be580693f46b15b0359d0a0b2991bf
gracelove <govlmo91@gmail.com>
2020-05-22 08:06:44 +0900

    in memory 유저

---
+++ src/main/java/kr/gracelove/tacocloud/tacos/security/SecurityConfig.java
@@ -0,0 +1,39 @@
+package kr.gracelove.tacocloud.tacos.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by GraceLove on 2020/05/22.
+ * Contact : govlmo91@gmail.com
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/design", "/orders")
+                .access("hasRole('ROLE_USER')")
+                .antMatchers("/", "/**").access("permitAll")
+                .and()
+                .httpBasic();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user1")
+                .password("{noop}password1")
+                .authorities("ROLE_USER")
+                .and()
+                .withUser("user2")
+                .password("{noop}password2")
+                .authorities("ROLE_USER");
+    }
+}
